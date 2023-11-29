26632f24f6223ebca8af9b4fde1c68d2a66c861b
martin.marx <martin.marx@quandoo.com>
2023-11-29 17:59:22 +0100

    Refactor a bit and remove some files

--- task2.3.py
+++ task2.3.py
@@ -4,45 +4,72 @@ import os
 
 from constants import names_table_name
 
-# today = datetime.date.today()
-today = datetime.datetime(2019, 1, 10)
-dates = [today - datetime.timedelta(days=x) for x in range(7)]
+initial_dates = [datetime.datetime(2019, 1, 10),
+                 datetime.datetime(2019, 1, 11),
+                 datetime.datetime(2019, 1, 12),
+                 datetime.datetime(2019, 3, 12),
+                 datetime.datetime(2019, 4, 12)]
 
-existing_data_queries = [
-    f"SELECT user_name FROM read_parquet('{names_table_name}/year={date.year}/month={date.month}/day={date.day}/*.parquet')"
-    for date
-    in dates
-    if os.path.isdir(f"{names_table_name}/year={date.year}/month={date.month}/day={date.day}")]
-
-existing_data_query = "\nUNION\n".join(existing_data_queries)
 
 # 2.3
-if not existing_data_query:
-    write_query = f"""SELECT (DATE '{today}') AS date, 0 AS number_active_users, 0 AS percentage_active_users"""
-else:
-    write_query = f"""
-  WITH active_users AS (
-    {existing_data_query}
-  ),
-  active_user_counter AS (
+def get_the_result_script(initial_date):
+    dates = [initial_date - datetime.timedelta(days=x) for x in range(7)]
+
+    existing_data_queries = [
+        f"SELECT user_name FROM read_parquet('{names_table_name}/year={date.year}/month={date.month}/day={date.day}/*.parquet')"
+        for date
+        in dates
+        if os.path.isdir(f"{names_table_name}/year={date.year}/month={date.month}/day={date.day}")]
+
+    existing_data_query = "\nUNION\n".join(existing_data_queries)
+
+    if not existing_data_query:
+        write_query = f"""SELECT (DATE '{initial_date}') AS date, 0 AS number_active_users, 0 AS percentage_active_users"""
+    else:
+        write_query = f"""
+    WITH active_users AS (
+      {existing_data_query}
+    ),
+    active_user_counter AS (
+      SELECT 
+        COUNT(DISTINCT user_name) AS active_user_count
+      FROM
+        active_users
+    ),
+    all_users_counter AS (
+      SELECT  
+        COUNT(DISTINCT user_name) AS all_user_count 
+      FROM 
+        read_parquet('{names_table_name}/**/*.parquet')
+    )
     SELECT 
-      COUNT(DISTINCT user_name) AS active_user_count
+      (DATE '{initial_date}') AS date ,
+      active_user_count AS number_active_users,
+      (active_user_count / all_user_count) AS percentage_active_users
     FROM
-      active_users
-  ),
-  all_users_counter AS (
-    SELECT  
-      COUNT(DISTINCT user_name) AS all_user_count 
-    FROM 
-      read_parquet('{names_table_name}/**/*.parquet')
-  )
-  SELECT 
-    (DATE '{today}') AS date ,
-    active_user_count AS number_active_users,
-    (active_user_count / all_user_count) AS percentage_active_users
-  FROM
-    active_user_counter, all_users_counter
-  ORDER BY 
-    date
-  """
-duckdb.sql(write_query).show()
+      active_user_counter, all_users_counter
+    ORDER BY 
+      date
+    """
+
+    return write_query
+
+
+queries = [f"""({get_the_result_script(initial_date)})""" for initial_date in initial_dates]
+if queries:
+    combined_query = "\nUNION ALL\n".join(queries)
+    final_query = f"""
+          WITH data AS (
+            {combined_query}
+          )
+          SELECT
+            date,
+            number_active_users,
+            percentage_active_users
+          FROM
+            data
+          ORDER BY
+            date
+          
+    """
+    duckdb.sql(final_query).show()
